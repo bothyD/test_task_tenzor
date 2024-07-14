45189c458c87dcaf712b45946a68de283bfdf4c6
bothyD <75413261+bothyD@users.noreply.github.com>
2024-07-14 22:02:44 +0700

    first promt end

--- conftest.py
+++ conftest.py
@@ -6,6 +6,6 @@ from selenium.webdriver.chrome.options import Options
 def browser():
     options = Options()
     options.add_argument('--headless')
-    chrome_browser = webdriver.Chrome()
+    chrome_browser = webdriver.Chrome(options)
     chrome_browser.implicitly_wait(10)
     return chrome_browser
