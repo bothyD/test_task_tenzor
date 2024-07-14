20702aa6018c5a4143aa7290c358fb7db603d32b
bothyD <75413261+bothyD@users.noreply.github.com>
2024-07-15 01:11:22 +0700

    edit promt2

--- tests/test_web_promt2.py
+++ tests/test_web_promt2.py
@@ -1,25 +1,25 @@
 from pages.sbis_page import SibsPage
 
-# def test_check_current_region(browser):
-#     sibsPage = SibsPage(browser)
-#     current_region = sibsPage.check_region()
-#     assert current_region == "Новосибирская обл."
+def test_check_current_region(browser):
+    sibsPage = SibsPage(browser)
+    current_region = sibsPage.check_region()
+    assert current_region == "Новосибирская обл."
 
-# def test_check_current_region(browser):
-#     sibsPage = SibsPage(browser)
-#     assert sibsPage.check_list_partners()
+def test_check_partners(browser):
+    sibsPage = SibsPage(browser)
+    assert sibsPage.check_list_partners()
 
-# def test_check_title_after_change_region(browser):
-#     sibsPage = SibsPage(browser)
-#     assert sibsPage.check_title() == 'СБИС Контакты — Камчатский край'
+def test_check_title_after_change_region(browser):
+    sibsPage = SibsPage(browser)
+    assert sibsPage.check_title() == 'СБИС Контакты — Камчатский край'
 
-# def test_check_url_after_change_region(browser):
-#     sibsPage = SibsPage(browser)
-#     assert sibsPage.check_url() == 'https://sbis.ru/contacts/41-kamchatskij-kraj?tab=clients'
+def test_check_url_after_change_region(browser):
+    sibsPage = SibsPage(browser)
+    assert sibsPage.check_url() == 'https://sbis.ru/contacts/41-kamchatskij-kraj?tab=clients'
 
-# def test_check_region_after_change_region(browser):
-#     sibsPage = SibsPage(browser)
-#     assert sibsPage.check_new_region() == "Камчатский край"
+def test_check_region_after_change_region(browser):
+    sibsPage = SibsPage(browser)
+    assert sibsPage.check_new_region() == "Камчатский край"
 
 def test_check_partners_after_change_region(browser):
     sibsPage = SibsPage(browser)
